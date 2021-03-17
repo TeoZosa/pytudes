fc1c5d5f33437d787c6fddef41e754cb06b20964
Teo Zosa <teofilo@sonosim.com>
2021-03-16 23:48:58 -0700

    :art: Update logic correctness and implicit communication

--- leetcode/educative/In-PlaceReversalOfALinkedList/2__reverse_a_sublist__medium.py
+++ leetcode/educative/In-PlaceReversalOfALinkedList/2__reverse_a_sublist__medium.py
@@ -4,6 +4,10 @@ from leetcode.utils.linked_list import NodeType, convert_list_to_linked_list
 
 
 def reverse_sub_list(head: NodeType, p: int, q: int) -> NodeType:
+    return _reverse_sub_list(head, p, q)
+
+
+def _reverse_sub_list(head: NodeType, start_pos: int, end_pos: int) -> NodeType:
     """
 
     Given the head of a LinkedList and two positions ‘p’ and ‘q’,
@@ -20,12 +24,10 @@ def reverse_sub_list(head: NodeType, p: int, q: int) -> NodeType:
         >>> head = convert_list_to_linked_list([1,2,3,4,5])
         >>> head.as_list()
         [1, 2, 3, 4, 5]
-        >>> head = reverse_sub_list(head,2,4)
+        >>> head = _reverse_sub_list(head,2,4)
         >>> head.as_list()
         [1, 4, 3, 2, 5]
     """
-    start_pos, end_pos = p, q  # Readable aliases
-
     ## Edge Cases ##
     if not head:  # Vacuous linked list
         return head
@@ -33,45 +35,37 @@ def reverse_sub_list(head: NodeType, p: int, q: int) -> NodeType:
     if start_pos < 1:  # Not 1-indexed
         return head
 
-    if start_pos >= end_pos:  # Invalid range
+    if end_pos <= start_pos:  # Invalid range
         return head
 
     """Algorithm"""
-    ## Initialize ##
-
-    # vars
+    ## INITIALIZE vars ##
     curr_pos = 1
 
-    ## FIND sublists 1 & 2 ##
-    # GET TAIL sublist1
-    # GET HEAD sublist2
+    ## FIND sublist 1 & 2 ##
     curr, prev = head, None
     while curr is not None:
-        if curr_pos == start_pos:
-            ## Capture the sublist1 tail, sublist2 head
+        if curr_pos == start_pos:  # Capture the sublist1 tail, sublist2 head
             s1_tail, s2_head = prev, curr
             break
         prev, curr = curr, curr.nxt
         curr_pos += 1
-    else:  # Invalid start_pos: exceeds list size
+    else:  # INVALID: start_pos > list size
         return head
 
-    ## REVERSE sublist2 < HEAD sublist3 ##
+    ## REVERSE sublist2 (< HEAD sublist3) ##
     curr, prev = s2_head, None
     while curr is not None and start_pos <= curr_pos <= end_pos:
         curr.nxt, prev, curr = prev, curr, curr.nxt
         curr_pos += 1
-    # Post-condition: curr_pos > end_pos XOR end_pos > list size
+    # POST-CONDITION: list size < end_pos XOR end_pos < curr_pos
+    s3_head, s2_reversed_head = curr, prev
+    s2_reversed_tail = s2_head  # Readable alias
 
     ## RE-LINK sublists ##
-    # GET HEAD sublist3
-    s3_head = curr  # None if end_pos > list size
-    # GET TAIL/HEAD REVERSED sublist2
-    s2_reversed_head, s2_reversed_tail = prev, s2_head  # Readable alias
-    # LINK s1 to r_s2
-    s1_tail.nxt = s2_reversed_head
-    # LINK r_s2 to s3
-    s2_reversed_tail.nxt = s3_head
+    s1_tail.nxt = s2_reversed_head  # LINK s1 to r_s2
+    s2_reversed_tail.nxt = s3_head  # LINK r_s2 to s3
+
     return head
 
 
@@ -80,6 +74,6 @@ def main():
     orig_vals = head.as_list()
     print(f"{orig_vals=}")
 
-    head = reverse_sub_list(head, 2, 4)  # Update local head ptr
+    head = _reverse_sub_list(head, 2, 4)  # Update local head ptr
     reversed_vals = head.as_list()
     print(f"{reversed_vals=}")
