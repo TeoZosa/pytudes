c24c8a76cd4cd96d1ca50633e5b575406cee3231
Teo Zosa <teofilo@sonosim.com>
2021-03-18 17:56:55 -0700

    :art: Update comments

--- leetcode/leetcode/medium/closest_k_points_to_origin/sort.py
+++ leetcode/leetcode/medium/closest_k_points_to_origin/sort.py
@@ -1,18 +1,21 @@
 """
-Sorting - O(NlogN); Space: O(N)
-  since we need to sort the entire list of points
-
-
+https://leetcode.com/problems/k-closest-points-to-origin/
 """
 
 
 class Solution:
     def kClosest(self, points: list[list[int]], K: int) -> list[list[int]]:
         """
-        >>> Solution().kClosest([[1,3],[-2,2]], 1)
-        [[-2, 2]]
-        >>> Solution().kClosest([[3,3],[5,-1],[-2,4]], 2)
-        [[3, 3], [-2, 4]]
+        Complexity:
+            K = len(heap)
+                Time: O(NlogN) to sort entire list of points
+                Space: O(N) (worst case) ; O(1) (best case)
+                    Since Python sort is Timsort
+        Examples:
+            >>> Solution().kClosest([[1,3],[-2,2]], 1)
+            [[-2, 2]]
+            >>> Solution().kClosest([[3,3],[5,-1],[-2,4]], 2)
+            [[3, 3], [-2, 4]]
         """
         compute_distance = lambda point: sum([coord ** 2 for coord in point])
         points.sort(key=compute_distance)
