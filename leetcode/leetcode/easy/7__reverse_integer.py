8eeaaf31c4df6a2255727a58f8fd689e252cc1c8
Teo Zosa <teofilo@sonosim.com>
2021-03-25 14:35:24 -0700

    :art: Move `_reverse` up, make main solution function

--- leetcode/leetcode/easy/7__reverse_integer.py
+++ leetcode/leetcode/easy/7__reverse_integer.py
@@ -13,38 +13,7 @@ See Also:
 
 class Solution:
     def reverse(self, x: int) -> int:
-        return _reverse_str_to_int(x)
-
-
-def _reverse_str_to_int(x: int) -> int:
-    """
-
-    Args:
-        x: 32-bit signed integer
-    Returns: x with digits reversed if result could be represented by 32 bits,
-             0 otherwise
-    Examples:
-        >>> _reverse_str_to_int(123)
-        321
-        >>> _reverse_str_to_int(-123)
-        -321
-        >>> _reverse_str_to_int(120)
-        21
-        >>> _reverse_str_to_int(0)
-        0
-        >>> _reverse_str_to_int(1534236469)
-        0
-    """
-
-    """ALGORITHM"""
-    ## INITIALIZE VARS ##
-    sign = 1 if x >= 0 else -1
-
-    ## REVERSE positive string version and reapply the original sign
-    reversed_x = sign * int(str(abs(x))[::-1])
-
-    ## Return reversed_x if it can be represented as a 32-bit signed integer
-    return reversed_x if -(2 ** 31) <= reversed_x <= (2 ** 31) - 1 else 0
+        return _reverse(x)
 
 
 def _reverse(x: int) -> int:
@@ -86,3 +55,34 @@ def _reverse(x: int) -> int:
         x_pos //= base  # Shift digits right, truncating off decimal
 
     return sign * reversed_x_pos
+
+
+def _reverse_str_to_int(x: int) -> int:
+    """
+
+    Args:
+        x: 32-bit signed integer
+    Returns: x with digits reversed if result could be represented by 32 bits,
+             0 otherwise
+    Examples:
+        >>> _reverse_str_to_int(123)
+        321
+        >>> _reverse_str_to_int(-123)
+        -321
+        >>> _reverse_str_to_int(120)
+        21
+        >>> _reverse_str_to_int(0)
+        0
+        >>> _reverse_str_to_int(1534236469)
+        0
+    """
+
+    """ALGORITHM"""
+    ## INITIALIZE VARS ##
+    sign = 1 if x >= 0 else -1
+
+    ## REVERSE positive string version and reapply the original sign
+    reversed_x = sign * int(str(abs(x))[::-1])
+
+    ## Return reversed_x if it can be represented as a 32-bit signed integer
+    return reversed_x if -(2 ** 31) <= reversed_x <= (2 ** 31) - 1 else 0
