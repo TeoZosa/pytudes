ca226e1b4194634622ae25f1cee91b4df02ec722
Teo Zosa <teofilo@sonosim.com>
2021-03-26 17:50:14 -0700

    :bug: Adhere to 32-bit integer constraint
    Signed-off-by: Teo Zosa <teofilo@sonosim.com>

--- leetcode/leetcode/easy/7__reverse_integer.py
+++ leetcode/leetcode/easy/7__reverse_integer.py
@@ -34,26 +34,38 @@ def _reverse(x: int) -> int:
         0
         >>> _reverse(1534236469)
         0
+        >>> _reverse(7463847412)
+        2147483647
+        >>> _reverse(-8463847412)
+        -2147483648
+        >>> _reverse(8463847412)
+        0
     """
 
     """ALGORITHM"""
     ## INITIALIZE VARS ##
-    base, sign = 10, 1 if x >= 0 else -1
+    number_base, max_and_min_val_prefix = 10, 214748364
+    is_negative = x < 0
 
     ## MIGRATE DIGITS from x_pos to reversed_x_pos
     x_pos, reversed_x_pos = abs(x), 0
 
     while x_pos > 0:
-        # Pretend environment can only store 32 bit integers =>
-        # exit loop early if we are about to overflow the result integer
-        if len(bin(sign * reversed_x_pos)) == 32:
-            reversed_x_pos = 0
-            break
+        digit = x_pos % number_base
+        x_pos //= number_base  # Shift digits right, truncating off decimal
+
+        # Exit early if 32-bit min/max bounds are about to be exceeded
+        if (
+            reversed_x_pos > max_and_min_val_prefix
+            or reversed_x_pos == max_and_min_val_prefix
+            and ((is_negative and digit > 8) or (not is_negative and digit > 7))
+        ):
+            return 0
 
         # Shift digits left and add new digit
-        reversed_x_pos = reversed_x_pos * base + x_pos % base
-        x_pos //= base  # Shift digits right, truncating off decimal
+        reversed_x_pos = reversed_x_pos * number_base + digit
 
+    sign = -1 if is_negative else 1
     return sign * reversed_x_pos
 
 
