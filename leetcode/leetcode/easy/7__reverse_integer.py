917b204829dd140192560b41a83421c9068f29a5
Teo Zosa <teofilo@sonosim.com>
2021-03-27 15:21:58 -0700

    :art: Refactor for better implicit documentation

--- leetcode/leetcode/easy/7__reverse_integer.py
+++ leetcode/leetcode/easy/7__reverse_integer.py
@@ -45,8 +45,6 @@ def _reverse(x: int) -> int:
     """ALGORITHM"""
     ## INITIALIZE VARS ##
     number_base, max_and_min_val_prefix = 10, 214748364
-    is_negative = x < 0
-
     ## MIGRATE DIGITS from x_pos to reversed_x_pos
     x_pos, reversed_x_pos = abs(x), 0
 
@@ -54,18 +52,19 @@ def _reverse(x: int) -> int:
         digit = x_pos % number_base
         x_pos //= number_base  # Shift digits right, truncating off decimal
 
-        # Exit early if 32-bit min/max bounds are about to be exceeded
-        if (
-            reversed_x_pos > max_and_min_val_prefix
-            or reversed_x_pos == max_and_min_val_prefix
-            and ((is_negative and digit > 8) or (not is_negative and digit > 7))
-        ):
+        # CHECK if reversal would cause 32-bit overflow
+        if reversed_x_pos > max_and_min_val_prefix:  # GUARANTEED overflow
             return 0
+        elif reversed_x_pos == max_and_min_val_prefix:  # MAY overflow
+            will_be_negative_overflow = x < 0 and digit > 8  # < -214748364_8
+            will_be_positive_overflow = x >= 0 and digit > 7  # > 214748364_7
+            if will_be_negative_overflow or will_be_positive_overflow:
+                return 0
 
         # Shift digits left and add new digit
         reversed_x_pos = reversed_x_pos * number_base + digit
 
-    sign = -1 if is_negative else 1
+    sign = -1 if x < 0 else 1
     return sign * reversed_x_pos
 
 
