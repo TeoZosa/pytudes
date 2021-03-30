ff52dc89433129a3881c5bb9f3a2cceb7d3ad1b1
Teo Zosa <teofilo@sonosim.com>
2021-03-30 08:59:01 -0700

    :sparkles: Add concise but less space efficient `_mergesort`

--- leetcode/Miscellany/sorting/mergesort.py
+++ leetcode/Miscellany/sorting/mergesort.py
@@ -1,6 +1,3 @@
-import copy
-
-
 def mergesort(nums: list, in_place: bool = True) -> list:
     """
     Complexity:
@@ -16,13 +13,69 @@ def mergesort(nums: list, in_place: bool = True) -> list:
         [-4, -4, -4, -2, -2, -2, 0, 0, 0, 2, 2, 2, 4, 4, 4]
     """
     if not in_place:
-        nums = copy.deepcopy(nums)
-    _mergesort_space_optimized(nums, 0, len(nums) - 1)
-    return nums
+        return _mergesort(nums)
+    else:
+        _mergesort_space_optimized(nums, 0, len(nums) - 1)
+        return nums
+
+
+def _mergesort(nums: list) -> list:
+    """
+    Complexity:
+        n = len(nums)
+            Space: 2n = O(n)
+                for (2 * n/2) copies of sorted left/right subarrays
+                and (1 * n) copy of merged array
+    """
+    ## EDGE CASES ##
+    if not nums:
+        return nums
+
+    """Algorithm"""
+    ## BASE CASE ##
+    if len(nums) <= 1:
+        return nums
+
+    ## INITIALIZE VARS##
+    mid = len(nums) // 2
+
+    ## RECURSIVELY SORT SUBARRAYS ##
+    left = _mergesort(nums[:mid])  # n/2 copy
+    right = _mergesort(nums[mid:])  # n/2 copy
+    return _merge(left, right)  # n copy
+
+
+def _merge(left: list, right: list) -> list:
+    ## INITIALIZE VARS ##
+    curr_left_idx = curr_right_idx = 0
+
+    # res
+    merged = []
+    ## Merge until a single subarray is exhausted
+    while curr_left_idx < len(left) and curr_right_idx < len(right):
+        if left[curr_left_idx] <= right[curr_right_idx]:
+            merged.append(left[curr_left_idx])
+            curr_left_idx += 1
+        else:
+            merged.append(right[curr_right_idx])
+            curr_right_idx += 1
+
+    ## Merge remainder of non-exhausted subarray
+    if curr_left_idx == len(left):  # left exhausted <=> right not exhausted
+        merged.extend(right[curr_right_idx:])
+    else:  # right exhausted <=> left not exhausted
+        merged.extend(left[curr_left_idx:])
+
+    return merged
 
 
 def _mergesort_space_optimized(nums: list, start: int, end: int) -> None:
-
+    """Performing merge operation in-place by overwriting associated indexes of input array
+    Complexity:
+        n = len(nums)
+            Space: n = O(n)
+                for (2 * n/2) copies of sorted left/right subarrays
+    """
     ## EDGE CASES ##
     if not nums:
         return
