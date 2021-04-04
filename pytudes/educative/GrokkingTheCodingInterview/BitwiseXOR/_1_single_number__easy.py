65081e20fee1dc6b37b3e92b87d3adcb2714b4f2
Teo Zosa <teofilo@sonosim.com>
2021-04-04 13:03:17 -0700

    :sparkles: Add `_1_single_number__easy.py`

---
+++ pytudes/educative/GrokkingTheCodingInterview/BitwiseXOR/_1_single_number__easy.py
@@ -0,0 +1,32 @@
+"""https://www.educative.io/courses/grokking-the-coding-interview/gk20xz4VwpG
+
+"""
+
+
+def find_single_number(arr: list[int]) -> int:
+    """Finds the single number in a non-empty array of integers where every number appears exactly twice, except for one which appears exactly once.
+
+    Complexity:
+        Time: O(n)
+        Space: O(1)
+
+    Args:
+        arr: array of numbers with all but a single element appearing exactly twice,
+            the single element that does not appear two times appears exactly once
+
+    Returns: the single element that appears exactly once
+
+    Examples:
+        >>> find_single_number([1, 4, 2, 1, 3, 2, 3])
+        4
+    """
+    """ALGORITHM"""
+    ## INITIALIZE VARS ##
+    single_number = 0
+
+    # XOR of all values in arr:
+    #   all duplicate elements will cancel out
+    #   leaving `single_number ^ 0` = `single_number`
+    for num in arr:  # REDUCE
+        single_number ^= num
+    return single_number
