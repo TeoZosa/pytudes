7dadd343ad5080cb4bf69d31f7ca16a1ff343958
Teo Zosa <teofilo@sonosim.com>
2021-04-05 13:50:54 -0700

    :sparkles: Add `_217__contains_duplicate.py`

---
+++ pytudes/leetcode/easy/_217__contains_duplicate.py
@@ -0,0 +1,42 @@
+"""https://leetcode.com/problems/contains-duplicate/
+
+
+    Examples:
+        >>> Solution().containsDuplicate([1,2,3])
+        False
+
+    Categories:
+        Array
+        Blind 75
+"""
+
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        return contains_duplicate(nums)
+
+
+def contains_duplicate(nums: list[int]) -> bool:
+    """
+
+    Args:
+        nums: array of possibly non-distinct integers
+
+    Returns: True if `nums` contains duplicate elements, False otherwise
+
+    Examples:
+        >>> contains_duplicate([1,2,3,1])
+        True
+        >>> contains_duplicate([1,2,3,4])
+        False
+        >>> contains_duplicate([1,1,1,3,3,4,3,2,4,2])
+        True
+    """
+    hash_table = {}
+    for num in nums:
+        if hash_table.get(num) is None:
+            hash_table[num] = True
+        else:
+            return True
+    else:
+        return False
